6c356258552de49fc99b8f2ab1436becc1e579ca
Saran440 <saranl@ecosoft.co.th>
2020-12-16 16:18:23 +0700

    [ADD] budget_activity_monitoring_report

---
+++ budget_activity_monitoring_report/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Budget Activity Monitoring",
+    "summary": "Monitoring budget with Activity",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-budgeting",
+    "depends": [
+        "budget_activity_purchase",
+        "budget_activity_purchase_request",
+        "budget_activity_expense"
+    ],
+    "data": [
+        "report/budget_monitor_report_view.xml",
+    ],
+    "maintainers": ["Saran440"],
+    "development_status": "Alpha",
+}
