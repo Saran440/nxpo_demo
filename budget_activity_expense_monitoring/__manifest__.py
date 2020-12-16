5e268d22a189530851bc9c71ab69202dea31dc7a
Saran440 <saranl@ecosoft.co.th>
2020-12-16 18:30:11 +0700

    [ADD] budget_activity_monitoring

---
+++ budget_activity_expense_monitoring/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Budget Activity Expense Monitoring",
+    "summary": "Monitoring budget activity with Expense",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-budgeting",
+    "depends": [
+        "budget_activity_monitoring",
+        "budget_control_expense",
+        "budget_activity_expense"
+    ],
+    "data": [],
+    "maintainers": ["Saran440"],
+    "development_status": "Alpha",
+}
--- budget_activity_purchase_request_monitoring/__manifest__.py
+++ budget_activity_purchase_request_monitoring/__manifest__.py
@@ -2,20 +2,18 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Budget Activity Monitoring",
-    "summary": "Monitoring budget with Activity",
+    "name": "Budget Activity Purchase Request Monitoring",
+    "summary": "Monitoring budget activity with Purchase Request",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-budgeting",
     "depends": [
-        "budget_activity_purchase",
-        "budget_activity_purchase_request",
-        "budget_activity_expense"
+        "budget_activity_monitoring",
+        "budget_control_purchase_request",
+        "budget_activity_purchase_request"
     ],
-    "data": [
-        "report/budget_monitor_report_view.xml",
-    ],
+    "data": [],
     "maintainers": ["Saran440"],
     "development_status": "Alpha",
 }
