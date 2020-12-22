22d59c8d3d0f3a186152b07ecf6d9c0335f0d1d1
Saran440 <saranl@ecosoft.co.th>
2020-12-22 17:40:34 +0700

    [ADD] revision_monitoring_project

--- nxpo_budget_revision_monitoring_project/__manifest__.py
+++ nxpo_budget_revision_monitoring_project/__manifest__.py
@@ -2,21 +2,18 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "NxPO - Project Monitoring",
-    "summary": "Budget Monitoring with Project",
+    "name": "NxPO Budget Revision Monitoring - Project",
+    "summary": "Budget Revision Monitoring with Project",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-budgeting",
     "depends": [
-        "budget_control",
-        "budget_monitoring_operating_unit",
-        "budget_monitoring_operating_unit_purchase",
-        "budget_monitoring_operating_unit_purchase_request",
-        "budget_monitoring_operating_unit_expense",
+        "budget_control_revision_department_monitoring",
+        "nxpo_res_project_analytic",
     ],
     "data": [
-        "report/budget_monitor_report_view.xml",
+        "report/budget_monitor_revision_view.xml",
     ],
     "maintainers": ["Saran440"],
     "development_status": "Alpha",
