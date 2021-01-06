8b440beb7f6b35f3d87300c69b9a5d8df6b37f44
Saran440 <saranl@ecosoft.co.th>
2021-01-06 18:07:57 +0700

    [FIX] AG

--- budget_activity_group/models/budget_activity.py
+++ budget_activity_group/models/budget_activity.py
@@ -1,6 +1,6 @@
 # Copyright 2020 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class BudgetActivity(models.Model):
@@ -12,9 +12,17 @@ class BudgetActivity(models.Model):
     )
     account_id = fields.Many2one(
         comodel_name="account.account",
-        # compute="_compute_account_id",
-        related="activity_group_id.account_id",
+        compute="_compute_account_id",
         store=True,
         readonly=False,
         required=False,
     )
+
+    @api.depends("activity_group_id")
+    def _compute_account_id(self):
+        for rec in self:
+            rec.account_id = (
+                not rec.account_id
+                and rec.activity_group_id.account_id
+                or rec.account_id
+            )
